023f6de780e3a715c8a6325b2c6ba2c412375c13
Parmar <Shalin.Parmar@unisys.com>
2018-09-24 10:09:37 +1200

    connected to spotify instead of mock data.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,6 @@
 import React, { Component } from 'react';
+import queryString from 'query-string';
 import './App.css';
-
-let fakeServerData = {
-  user: {
-    name: 'Shalin',
-    playlists: [
-      {
-        name: 'Prabhatiya',
-        songs: [{ name: 'Dhyan Dhar', duration: 800}, {name: 'Jago Mohan Pyara', duration: 680}, {name: 'Prit Kar', duration: 760}]
-      },
-      {
-        name: 'Sandhya Aarti',
-        songs: [{name: 'Jai Sadguru Swami', duration: 300}, {name: 'Ram Krishna Govind', duration: 220}, {name: 'Anant Kotindu', duration: 310}, {name: 'Nirvikalp Uttam Ati', duration: 175}, {name: 'Krupa Karo', duration: 320}]
-      },
-      {
-        name: 'Thal',
-        songs: [{name: 'Jamone Jamadu Re', duration: 380}, {name: 'Mare Gher Aavjo Chogladhari', duration: 155}, {name: 'Avinashi Avo Re', duration: 1600}]
-      },
-      {
-        name: 'Chesta',
-        songs: [{name: 'Pratham Shr Hari Ne', duration: 1000}, {name: 'Ora Avo Shyam Snehi', duration: 1820}, {name: 'Podho Podho Sahjanand Swami', duration: 290}, {name: 'Mitha Vhala Kem Visaru', duration: 245}, {name: 'Akshar Na Vasi Vhalo', duration: 460}]
-      }
-    ]
-  }
-}
 
 class PlaylistCounter extends Component {
   render() {
@@ -66,7 +43,7 @@ class Playlist extends Component {
     let playlist = this.props.playlist;
     return (
       <div style={{width: '25%', display: 'inline-block'}}>
-        <img />
+        <img src={playlist.imageUrl} style={{width: '60px'}} />
         <h3>{playlist.name}</h3>
         <ul>
           {playlist.songs.map(song => 
@@ -81,36 +58,50 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      serverData: {},
+      user: {},
+      playlists: [],
       filterString: ''
     }
   }
   componentDidMount() {
-    setTimeout(() =>
-      this.setState({
-        serverData: fakeServerData
-      }), 1000);
+    let parsed = queryString.parse(window.location.search);
+    let accessToken = parsed.access_token;
+
+    fetch('https://api.spotify.com/v1/me', { headers: { 'Authorization': 'Bearer ' + accessToken } })
+      .then(response => response.json())
+      .then(data => this.setState({ user: { name: data.display_name } }));
+
+    fetch('https://api.spotify.com/v1/me/playlists', { headers: { 'Authorization': 'Bearer ' + accessToken } })
+      .then(response => response.json())
+      .then(data => this.setState({ playlists: data.items.map(item => ({
+          name: item.name,
+          imageUrl: item.images[0].url,
+          songs: []
+        }))
+      }));
   }
 
   render() {
-    let playlistsToRender = this.state.serverData.user? this.state.serverData.user.playlists
-      .filter(
-        playlist => playlist.name.toLowerCase().includes(this.state.filterString.toLowerCase())
-      ) : [];
+    let playlistsToRender = this.state.user && this.state.playlists
+      ? this.state.playlists.filter(
+            playlist => playlist.name.toLowerCase().includes(this.state.filterString.toLowerCase())) 
+      : [];
       
     return (
       <div className="App">
-        {this.state.serverData.user ?
-        <div>
+        {this.state && this.state.user 
+        ?<div>
           <h1>
-              {this.state.serverData.user.name}'s Playlist
+              {this.state.user.name}'s Playlist
           </h1>
           <PlaylistCounter playlists={playlistsToRender} />
           <HoursCounter playlists={playlistsToRender} />
           <Filter onTextChange={text => this.setState({filterString: text})} />
           {playlistsToRender.map(playlist => 
             <Playlist playlist={playlist} />)}
-        </div> : <h1>Loading.....</h1>}
+          </div> 
+        : <button onClick={() => window.location = 'http://localhost:8888/login'} 
+              style={{padding: '20px', fontSize: '50px', marginTop: '20px'}}>Sign In with Spotify</button>}
       </div>
     )
   }
