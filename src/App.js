da39d183c18e6710abd3fd01fa3163c9db558e20
Parmar <Shalin.Parmar@unisys.com>
2018-09-24 11:38:00 +1200

    Added filter for songs along with playlists.

--- src/App.js
+++ src/App.js
@@ -114,8 +114,11 @@ class App extends Component {
 
   render() {
     let playlistsToRender = this.state.user && this.state.playlists
-      ? this.state.playlists.filter(
-            playlist => playlist.name.toLowerCase().includes(this.state.filterString.toLowerCase())) 
+      ? this.state.playlists.filter(playlist => {
+          let matchesPlaylist = playlist.name.toLowerCase().includes(this.state.filterString.toLowerCase());
+          let matchesSong = playlist.songs.find(song => song.name.toLowerCase().includes(this.state.filterString.toLowerCase()));
+          return matchesPlaylist || matchesSong;
+        })
       : [];
       
     return (
